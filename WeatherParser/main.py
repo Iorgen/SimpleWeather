74e8ec064049066f3294c5015cc3f0c52023366a
Jurgen Mayer <iorgenkras@gmail.com>
2019-08-16 16:47:02 +0700

    small fix

--- WeatherParser/main.py
+++ WeatherParser/main.py
@@ -4,9 +4,15 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'SimpleWeather.settings'
 django.setup()
 from ApiWeather.models import City
 from WeatherParser import parse_weather
+import time
 
 
 if __name__ == "__main__":
-    cities = City.get_cities_names()
-    cities = cities[::1]
-    parse_weather(cities)
+    while 1:
+        try:
+            cities = City.get_cities_names()
+            cities = cities[::1]
+            parse_weather(cities)
+            time.sleep(100)
+        except:
+            continue
